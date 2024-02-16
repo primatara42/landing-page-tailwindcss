9af93752ab9fd56161e33ccdb89b6102e1480c42
Prima Putra Agusantara <primatara42@gmail.com>
2024-02-17 00:17:48 +0700

    improvement header & navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,9 @@ module.exports = {
       '2xl': {'max': '1535px'},
       'xl': {'max': '1279px'},
       'lg': {'max': '1023px'},
-      'md': {'max': '767px'},
+      'md': {'max': '800px'},
       'sm': {'max': '639px'},
+      '-sm': {'min': '639px'}
     }
   },
   plugins: [],
